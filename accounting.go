ae1993eb8915c9607d6d716e990e9ec88ece95f9
EdLo <b93303015@gmail.com>
2021-01-17 22:31:44 +0800

    extract inline variable and change firstDay/lastDay input

--- accounting.go
+++ accounting.go
@@ -12,7 +12,10 @@ type Accounting struct {
 func (a *Accounting) TotalAmount(start time.Time, end time.Time) float64 {
 	budgets := a.BudgetRepo.GetAll()
 	if len(budgets) > 0 {
-		if start.After(a.lastDay(budgets)) || end.Before(a.firstDay(budgets)) {
+		budget := budgets[0]
+		firstDay := a.firstDay(budget)
+		lastDay := a.lastDay(budget)
+		if start.After(lastDay) || end.Before(firstDay) {
 			return 0
 		}
 		return end.Sub(start).Hours()/24 + 1
@@ -20,12 +23,12 @@ func (a *Accounting) TotalAmount(start time.Time, end time.Time) float64 {
 	return 0
 }
 
-func (a *Accounting) lastDay(budgets []Budget) time.Time {
-	budgetLastDate, _ := time.Parse("200601", budgets[0].YearMonth)
+func (a *Accounting) lastDay(budgets Budget) time.Time {
+	budgetLastDate, _ := time.Parse("200601", budgets.YearMonth)
 	return now.With(budgetLastDate).EndOfMonth()
 }
 
-func (a *Accounting) firstDay(budgets []Budget) time.Time {
-	budgetFirstDay, _ := time.Parse("200601", budgets[0].YearMonth)
+func (a *Accounting) firstDay(budgets Budget) time.Time {
+	budgetFirstDay, _ := time.Parse("200601", budgets.YearMonth)
 	return budgetFirstDay
 }
