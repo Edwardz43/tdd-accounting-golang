16419764232a5c7a9426fcb1cfcf81c794c92a42
EdLo <b93303015@gmail.com>
2021-01-17 20:58:14 +0800

    add budget repo for accounting

--- accounting.go
+++ accounting.go
@@ -3,6 +3,7 @@ package accounting
 import "time"
 
 type Accounting struct {
+	*BudgetRepo
 }
 
 func (a Accounting) TotalAmount(start time.Time, end time.Time) float64 {
--- accounting_test.go
+++ accounting_test.go
@@ -9,7 +9,7 @@ import (
 var account *accounting.Accounting
 
 func beforeEach() {
-	account = new(accounting.Accounting)
+	account = &accounting.Accounting{BudgetRepo: &accounting.BudgetRepo{}}
 }
 
 func createPeriod(startStr string, endStr string) (time.Time, time.Time) {
@@ -24,3 +24,14 @@ func TestNoBudgets(t *testing.T) {
 	assert.Equal(t, .0, account.TotalAmount(start, end))
 }
 
+type Budget struct {
+	YearMonth string
+	Amount int
+}
+
+func TestPeriodInWholeMonth(t *testing.T) {
+	beforeEach()
+	//account.
+	start, end := createPeriod("2021-04-01", "2021-04-30")
+	assert.Equal(t, 30.0, account.TotalAmount(start, end))
+}
