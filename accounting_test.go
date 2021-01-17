5452077fe06c7c0f209048376377c4d397750d06
EdLo <b93303015@gmail.com>
2021-01-17 15:41:18 +0800

    extract createPeriod func

--- accounting_test.go
+++ accounting_test.go
@@ -14,9 +14,15 @@ func (a Accounting) totalAmount(start time.Time, end time.Time) float64 {
 	return 0
 }
 
+func createPeriod(startStr string, endStr string) (time.Time, time.Time) {
+	start, _ := time.Parse("2006-01-02", startStr)
+	end, _ := time.Parse("2006-01-02", endStr)
+	return start, end
+}
+
 func TestNoBudgets(t *testing.T) {
 	account := new(Accounting)
-	start, _ := time.Parse("2006-01-02", "2021-04-02")
-	end , _:= time.Parse("2006-01-02", "2021-04-02")
+	start, end := createPeriod("2021-04-02", "2021-04-02")
 	assert.Equal(t, .0, account.totalAmount(start, end))
 }
+
