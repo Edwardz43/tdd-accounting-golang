c115975a4fb1cb4ddb0cdce6c6914dc4f9bd3803
EdLo <b93303015@gmail.com>
2021-01-17 15:37:36 +0800

    add start and end parameters for totalAmount func

--- accounting_test.go
+++ accounting_test.go
@@ -3,17 +3,20 @@ package account_test
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 type Accounting struct {
 
 }
 
-func (a Accounting) totalAmount() float64 {
+func (a Accounting) totalAmount(start time.Time, end time.Time) float64 {
 	return 0
 }
 
 func TestNoBudgets(t *testing.T) {
 	account := new(Accounting)
-	assert.Equal(t, .0, account.totalAmount())
+	start, _ := time.Parse("2006-01-02", "2021-04-02")
+	end , _:= time.Parse("2006-01-02", "2021-04-02")
+	assert.Equal(t, .0, account.totalAmount(start, end))
 }
