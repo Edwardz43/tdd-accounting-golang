8491501b79fa39a351ddaa23987f95eb1fa9fc89
EdLo <b93303015@gmail.com>
2021-01-17 15:26:48 +0800

    rename first test case name

--- accounting_test.go
+++ accounting_test.go
@@ -13,7 +13,7 @@ func (a Accounting) totalAmount() float64 {
 	return 0
 }
 
-func TestNoBudget(t *testing.T) {
+func TestNoBudgets(t *testing.T) {
 	account := new(Accounting)
 	assert.Equal(t, .0, account.totalAmount())
 }
