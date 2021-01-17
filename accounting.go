1b5ff79bff55c61f163d039dfc3c81bb59ba07fb
EdLo <b93303015@gmail.com>
2021-01-17 22:01:45 +0800

    extract firstDay func and inline variable

--- accounting.go
+++ accounting.go
@@ -11,12 +11,16 @@ type Accounting struct {
 func (a *Accounting) TotalAmount(start time.Time, end time.Time) float64 {
 	budgets := a.BudgetRepo.GetAll()
 	if len(budgets) > 0 {
-		budgetFirstDay, _ := time.Parse("200601", budgets[0].YearMonth)
-		if end.Before(budgetFirstDay) {
+		if end.Before(a.firstDay(budgets)) {
 			return 0
 		}
-		days := end.Sub(start).Hours() / 24 + 1
+		days := end.Sub(start).Hours()/24 + 1
 		return days
 	}
 	return 0
+}
+
+func (a *Accounting) firstDay(budgets []Budget) time.Time {
+	budgetFirstDay, _ := time.Parse("200601", budgets[0].YearMonth)
+	return budgetFirstDay
 }
